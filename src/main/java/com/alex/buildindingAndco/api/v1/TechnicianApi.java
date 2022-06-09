daa7e5e2759284c71aa07db9145ec6f41ec04131
Alex Dnl <deniel.alexandre@orange.fr>
2022-06-09 15:15:17 +0200

    rajout api vehicle

--- src/main/java/com/alex/buildindingAndco/api/v1/TechnicianApi.java
+++ src/main/java/com/alex/buildindingAndco/api/v1/TechnicianApi.java
@@ -1,9 +1,124 @@
 package com.alex.buildindingAndco.api.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alex.buildindingAndco.api.dto.TechnicianDto;
+import com.alex.buildindingAndco.exception.NotAllowedToDeleteException;
+import com.alex.buildindingAndco.exception.UnknownResourceException;
+import com.alex.buildindingAndco.mapper.TechnicianMapper;
+import com.alex.buildindingAndco.service.TechnicianService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/technicians")
 public class TechnicianApi {
+
+    Logger log = LoggerFactory.getLogger(TechnicianApi.class);
+
+    private final TechnicianService technicianService;
+    private final TechnicianMapper technicianMapper;
+
+    public TechnicianApi(TechnicianService technicianService, TechnicianMapper technicianMapper) {
+        this.technicianService = technicianService;
+        this.technicianMapper = technicianMapper;
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(
+            summary = "Return the list of all technicianes by city ascending.")
+    public ResponseEntity<List<TechnicianDto>> getAll() {
+        log.info("Retrieving Technicianes...");
+
+        return ResponseEntity.ok(
+                this.technicianService.getAll().stream()
+                        .map(this.technicianMapper::mapToDto)
+                        .toList()
+        );
+    }
+
+    @GetMapping(value = "/{id}")
+    @Operation(summary = "Trying to retrieve a technician from the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Return an technician found the given ID"),
+            @ApiResponse(responseCode = "404", description = "No technician found the given ID")
+    })
+    public ResponseEntity<TechnicianDto> getById(@PathVariable final Integer id) {
+        try {
+            return ResponseEntity.ok(
+                    this.technicianMapper
+                            .mapToDto(this.technicianService.getById(id)));
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        }
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE}
+    )
+    @Operation(summary = "Create a technician")
+    @ApiResponse(responseCode = "201", description = "Created")
+    public ResponseEntity<TechnicianDto> createTechnician(@RequestBody final TechnicianDto technicianDto) {
+        log.debug("Attempting to create technician with lastname {}", technicianDto.getLastName());
+
+        TechnicianDto technicianDtoResponse =
+                this.technicianMapper.mapToDto(
+                        this.technicianService.createTechnician(
+                                this.technicianMapper.mapToModel(technicianDto)
+                        ));
+
+        return ResponseEntity
+                .created(URI.create("/v1/technicians/" + technicianDtoResponse.getId()))
+                .body(technicianDtoResponse);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Delete a technician for the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "404", description = "No technician found the given ID"),
+            @ApiResponse(responseCode = "403", description = "Cannot delete the technician for the given ID")
+    })
+    public ResponseEntity<Void> deleteTechnician(@PathVariable final Integer id) {
+        log.debug("Attemtping to delete a Technician with id {}", id);
+        try {
+            this.technicianService.deleteTechnician(id);
+            return ResponseEntity.noContent().build();
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        } catch (NotAllowedToDeleteException ex) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
+    }
+
+    @PutMapping(path = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+    @Operation(summary = "Update a technician")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No content")
+    })
+    public ResponseEntity<Void> updateTechnician(@PathVariable final Integer id, @RequestBody TechnicianDto technicianDto) {
+        try {
+            log.debug("Updating technician {}", technicianDto.getId());
+            technicianDto.setId(id);
+            this.technicianService.updateTechnician(technicianMapper.mapToModel(technicianDto));
+            log.debug("Successfully updated technician {}", technicianDto.getId());
+
+            return ResponseEntity.noContent().build();
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/com/alex/buildindingAndco/api/v1/VehicleApi.java
+++ src/main/java/com/alex/buildindingAndco/api/v1/VehicleApi.java
@@ -1,9 +1,124 @@
 package com.alex.buildindingAndco.api.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alex.buildindingAndco.api.dto.VehicleDto;
+import com.alex.buildindingAndco.exception.NotAllowedToDeleteException;
+import com.alex.buildindingAndco.exception.UnknownResourceException;
+import com.alex.buildindingAndco.mapper.VehicleMapper;
+import com.alex.buildindingAndco.service.VehicleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/vehicles")
 public class VehicleApi {
+
+    Logger log = LoggerFactory.getLogger(VehicleApi.class);
+
+    private final VehicleService vehicleService;
+    private final VehicleMapper vehicleMapper;
+
+    public VehicleApi(VehicleService vehicleService, VehicleMapper vehicleMapper) {
+        this.vehicleService = vehicleService;
+        this.vehicleMapper = vehicleMapper;
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(
+            summary = "Return the list of all vehiclees by city ascending.")
+    public ResponseEntity<List<VehicleDto>> getAll() {
+        log.info("Retrieving vehiclees...");
+
+        return ResponseEntity.ok(
+                this.vehicleService.getAll().stream()
+                        .map(this.vehicleMapper::mapToDto)
+                        .toList()
+        );
+    }
+
+    @GetMapping(value = "/{id}")
+    @Operation(summary = "Trying to retrieve a vehicle from the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Return an vehicle found the given ID"),
+            @ApiResponse(responseCode = "404", description = "No vehicle found the given ID")
+    })
+    public ResponseEntity<VehicleDto> getById(@PathVariable final Integer id) {
+        try {
+            return ResponseEntity.ok(
+                    this.vehicleMapper
+                            .mapToDto(this.vehicleService.getById(id)));
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        }
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE}
+    )
+    @Operation(summary = "Create a vehicle")
+    @ApiResponse(responseCode = "201", description = "Created")
+    public ResponseEntity<VehicleDto> createVehicle(@RequestBody final VehicleDto vehicleDto) {
+        log.debug("Attempting to create vehicle with model {}", vehicleDto.getModel());
+
+        VehicleDto vehicleDtoResponse =
+                this.vehicleMapper.mapToDto(
+                        this.vehicleService.createVehicle(
+                                this.vehicleMapper.mapToModel(vehicleDto)
+                        ));
+
+        return ResponseEntity
+                .created(URI.create("/v1/vehicles/" + vehicleDtoResponse.getId()))
+                .body(vehicleDtoResponse);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Delete a vehicle for the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "404", description = "No vehicle found the given ID"),
+            @ApiResponse(responseCode = "403", description = "Cannot delete the vehicle for the given ID")
+    })
+    public ResponseEntity<Void> deleteVehicle(@PathVariable final Integer id) {
+        log.debug("Attemtping to delete a vehicle with id {}", id);
+        try {
+            this.vehicleService.deleteVehicle(id);
+            return ResponseEntity.noContent().build();
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        } catch (NotAllowedToDeleteException ex) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
+    }
+
+    @PutMapping(path = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+    @Operation(summary = "Update a vehicle")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No content")
+    })
+    public ResponseEntity<Void> updateVehicle(@PathVariable final Integer id, @RequestBody VehicleDto vehicleDto) {
+        try {
+            log.debug("Updating vehicle {}", vehicleDto.getId());
+            vehicleDto.setId(id);
+            this.vehicleService.updateVehicle(vehicleMapper.mapToModel(vehicleDto));
+            log.debug("Successfully updated vehicle {}", vehicleDto.getId());
+
+            return ResponseEntity.noContent().build();
+        } catch (UnknownResourceException ure) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ure.getMessage());
+        }
+    }
+
+
 }
